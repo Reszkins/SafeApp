8931e4303ca8807a2e3097cf6f8dff5bb215ed54
Oskar Reszka <oskarreszka12@gmail.com>
2023-01-08 23:12:18 +0100

    clientapp fix

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomersComponent } from './customers/customers.component';
 import { AuthGuard } from './guards/auth-guard.service';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -11,8 +10,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'notes', component: NotesComponent, canActivate: [AuthGuard]},
-  { path: 'customers', component: CustomersComponent, canActivate: [AuthGuard]}
+  { path: 'notes', component: NotesComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
